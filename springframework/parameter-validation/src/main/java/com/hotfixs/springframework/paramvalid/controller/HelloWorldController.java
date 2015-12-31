01fc70d840ea251f122915900862afdb8b52a89c
wangjunwei <i.junwei.wang@gmail.com>
2015-12-31 15:05:04 +0800

    Fix travis build error

--- springframework/parameter-validation/src/main/java/com/hotfixs/springframework/paramvalid/controller/HelloWorldController.java
+++ springframework/parameter-validation/src/main/java/com/hotfixs/springframework/paramvalid/controller/HelloWorldController.java
@@ -1,10 +1,13 @@
 package com.hotfixs.springframework.paramvalid.controller;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Size;
 
 /**
  * @author wangjunwei
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping(value = "/test", method = RequestMethod.POST)
     public String hello(
-            @RequestParam("id") int id,
+            @RequestParam("id") @NotEmpty @Size int id,
             @RequestParam("name") String name,
             @RequestParam("age") int age
             //            @RequestParam("birthday") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate birthday
